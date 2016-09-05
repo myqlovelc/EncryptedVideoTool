e3cb6e213281d517037469636bf189d69ecc09ad
myqlovelc <myqtmac@126.com>
2016-09-05 22:24:46 +0800

    Add README.md

---
+++ EncryptedVideoTool/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace EncryptedVideoTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.source_btn = new System.Windows.Forms.Button();
+            this.output_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.generate_btn = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // source_btn
+            // 
+            this.source_btn.Font = new System.Drawing.Font("宋体", 11F);
+            this.source_btn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.source_btn.Location = new System.Drawing.Point(270, 34);
+            this.source_btn.Name = "source_btn";
+            this.source_btn.Size = new System.Drawing.Size(317, 71);
+            this.source_btn.TabIndex = 0;
+            this.source_btn.Text = "选择视频文件";
+            this.source_btn.UseVisualStyleBackColor = true;
+            this.source_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // output_btn
+            // 
+            this.output_btn.Font = new System.Drawing.Font("宋体", 11F);
+            this.output_btn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.output_btn.Location = new System.Drawing.Point(270, 301);
+            this.output_btn.Name = "output_btn";
+            this.output_btn.Size = new System.Drawing.Size(317, 71);
+            this.output_btn.TabIndex = 1;
+            this.output_btn.Text = "选择文件夹";
+            this.output_btn.UseVisualStyleBackColor = true;
+            this.output_btn.Click += new System.EventHandler(this.output_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 11F);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(37, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "原始视频路径";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 11F);
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.Location = new System.Drawing.Point(42, 132);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(545, 145);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 11F);
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(37, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "输出文件目录";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 11F);
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(37, 591);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "输出文件名";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 11F);
+            this.textBox1.Location = new System.Drawing.Point(270, 580);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 41);
+            this.textBox1.TabIndex = 8;
+            // 
+            // generate_btn
+            // 
+            this.generate_btn.Font = new System.Drawing.Font("宋体", 11F);
+            this.generate_btn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.generate_btn.Location = new System.Drawing.Point(42, 661);
+            this.generate_btn.Name = "generate_btn";
+            this.generate_btn.Size = new System.Drawing.Size(545, 71);
+            this.generate_btn.TabIndex = 9;
+            this.generate_btn.Text = "生成视频";
+            this.generate_btn.UseVisualStyleBackColor = true;
+            this.generate_btn.Click += new System.EventHandler(this.generate_btn_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox2.Font = new System.Drawing.Font("宋体", 11F);
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox2.Location = new System.Drawing.Point(42, 399);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(545, 145);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "All files (*.*)|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 768);
+            this.Controls.Add(this.generate_btn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output_btn);
+            this.Controls.Add(this.source_btn);
+            this.Name = "Form1";
+            this.Text = "VideoTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button source_btn;
+        private System.Windows.Forms.Button output_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button generate_btn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ EncryptedVideoTool/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using System.Security.Cryptography;
+
+namespace EncryptedVideoTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string message = openFileDialog1.FileName;
+                richTextBox1.Text = message;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void output_btn_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string message = folderBrowserDialog1.SelectedPath;
+                richTextBox2.Text = message;
+            }
+        }
+
+        private void generate_btn_Click(object sender, System.EventArgs e)
+        {
+            if (richTextBox1.TextLength == 0 || richTextBox2.TextLength == 0 || textBox1.TextLength == 0)
+            {
+                MessageBox.Show("请将信息填写完整！", "VideoTool");
+            }
+
+            else
+            {
+                byte[] position_array = new byte[8];
+                byte[] mask_array = new byte[8];
+                byte[] key_array = new byte[56];
+
+                Random rand = new Random();
+
+                generateRandomPositions(position_array, rand);
+                generateRandomMasks(mask_array, rand);
+                generateRandomKey(key_array, rand);
+
+                for (int i = 0; i < position_array.Length; i++) {
+			        Console.Write(position_array[i] + " ");
+		        }
+                Console.WriteLine();
+		        for (int i = 0; i < mask_array.Length; i++) {
+			        Console.Write(mask_array[i] + " ");
+		        }
+                Console.WriteLine();
+
+                int transform_length = 256;
+
+                FileStream sFile = new FileStream(richTextBox1.Text, FileMode.Open);
+                byte[] byte_array = new byte[transform_length];
+                sFile.Read(byte_array, 0, transform_length);
+                for (int i = 0; i < byte_array.Length; i++)
+                {
+                    Console.Write(byte_array[i] + " ");
+                }
+                Console.WriteLine();
+
+                transformData(byte_array, transform_length, position_array, mask_array);
+
+                for (int i = 0; i < byte_array.Length; i++)
+                {
+                    Console.Write(byte_array[i] + " ");
+                }
+                Console.WriteLine();
+
+                string md5 = getMD5ByHashAlgorithm(byte_array, 0, transform_length).ToLower();
+                Console.Write(md5 + "\n");
+
+                FileStream dos = new FileStream(richTextBox2.Text + "/" + textBox1.Text + ".cglvr", FileMode.Create);
+
+                BinaryWriter bw = new BinaryWriter(dos);
+
+                bw.Seek(0, SeekOrigin.Begin);
+                long header_length = 116;
+                byte[] header_length_array = System.BitConverter.GetBytes((long)header_length);
+                Array.Reverse(header_length_array);
+                bw.Write(header_length_array);
+                byte[] transform_length_array = System.BitConverter.GetBytes((int)transform_length);
+                Array.Reverse(transform_length_array);
+                bw.Write(transform_length_array);
+
+                byte[] md5_array = System.Text.Encoding.Default.GetBytes(md5);
+                bw.Write(md5_array);
+
+                for (int i = 0; i < position_array.Length; i++)
+                {
+                    bw.Write(position_array[i]);
+                    bw.Write(mask_array[i]);
+                }
+
+                bw.Write(key_array);
+
+                bw.Write(byte_array);
+
+                sFile.Seek(transform_length, SeekOrigin.Begin);
+
+                int iBufferSize = 1024000;
+                byte[] buffer = new byte[iBufferSize];
+                int readLength = 0;//每次读取长度
+                while ((readLength = sFile.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    bw.Write(buffer, 0, readLength);
+                }
+
+                bw.Close();
+                dos.Close();
+                sFile.Close();
+
+                MessageBox.Show("生成视频成功！", "VideoTool");
+            }
+        }
+
+        private static void generateRandomMasks(byte[] mask_array, Random rand)
+        {
+            // TODO Auto-generated method stub
+            int length = mask_array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                mask_array[i] = (byte)rand.Next(256);
+            }
+        }
+
+        private static void generateRandomKey(byte[] key_array, Random rand)
+        {
+            // TODO Auto-generated method stub
+            int length = key_array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                key_array[i] = (byte)rand.Next(256);
+            }
+        }
+
+        private static void generateRandomPositions(byte[] position_array, Random rand)
+        {
+            // TODO Auto-generated method stub
+            int length = position_array.Length;
+            int i = 0;
+            while (i < length)
+            {
+                int r = rand.Next(256);
+                int j = 0;
+                while (j < i)
+                {
+                    if (position_array[j] == r)
+                    {
+                        break;
+                    }
+                    j++;
+                }
+
+                if (j == i)
+                {
+                    position_array[i] = (byte)r;
+                    i++;
+                }
+            }
+        }
+
+        private static void transformData(byte[] byte_array, int transform_length,
+            byte[] position_array, byte[] mask_array)
+        {
+
+            // 对随机抽取的单个字节进行位转换
+            for (int i = 0; i < position_array.Length; i++)
+            {
+                int position = position_array[i] & 0xff;
+                byte mask = mask_array[i];
+
+                byte current_byte = byte_array[position];
+                byte masked = (byte)(current_byte ^ mask);
+
+                byte temp1 = (byte)(masked << 7);
+                byte temp2 = (byte)(masked >> 1);
+                if (temp2 < 0)
+                {
+                    temp2 = (byte)(temp2 + 128);
+                }
+                byte transformed = (byte)(temp1 ^ temp2);
+
+                byte_array[position] = transformed;
+            }
+
+            // 对256个字节进行转换加密
+            for (int i = 0; i < (transform_length / 2); i++)
+            {
+                byte temp = byte_array[i];
+                byte_array[i] = byte_array[transform_length - 1 - i];
+                byte_array[transform_length - 1 - i] = temp;
+            }
+
+            byte tem = byte_array[transform_length - 1];
+            for (int i = transform_length - 1; i > 0; i--)
+            {
+                byte_array[i] = byte_array[i - 1];
+            }
+            byte_array[0] = tem;
+        }
+
+        //计算单个小文件的整个Md5
+        public static string getMD5ByHashAlgorithm(byte[] byte_array, int offset, int transform_length)
+        {
+            int bufferSize = transform_length;//自定义缓冲区大小16K
+            HashAlgorithm hashAlgorithm = new MD5CryptoServiceProvider();
+            var output = new byte[bufferSize];
+            hashAlgorithm.TransformBlock(byte_array, offset, transform_length, output, 0);
+            //完成最后计算，必须调用(由于上一部循环已经完成所有运算，所以调用此方法时后面的两个参数都为0)
+            hashAlgorithm.TransformFinalBlock(byte_array, 0, 0);
+            string md5 = BitConverter.ToString(hashAlgorithm.Hash);
+            hashAlgorithm.Clear();
+            md5 = md5.Replace("-", "");
+            return md5;
+        }
+    }
+}
